2533a5da401ee2e03fcce6644b086b8e55fc3ff8
Alberto Ponces <ponces26@gmail.com>
2023-03-27 01:20:38 +0100

    revert: aptX encoders were merged upstream by Qualcomm so we don't need this anymore

--- evo.mk
+++ evo.mk
@@ -12,9 +12,5 @@ TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
 TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2 := true
 
-PRODUCT_PACKAGES += \
-    libaptX_encoder \
-    libaptXHD_encoder
-
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_evo/tiramisu/ota.json
