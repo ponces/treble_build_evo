eb2ea6cebbc215afddec483d6186a89645c26435
Alberto Ponces <ponces26@gmail.com>
2023-05-09 00:19:37 +0100

    fix: Remove `base: Provide libnfc-nci` and `board: Add broken duplicate rules flag` patches and move build flags and instructions to our own makefile

--- evo.mk
+++ evo.mk
@@ -3,6 +3,8 @@ $(call inherit-product, vendor/evolution/config/BoardConfigSoong.mk)
 $(call inherit-product, device/evolution/sepolicy/common/sepolicy.mk)
 -include vendor/evolution/build/core/config.mk
 
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
 TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -11,6 +13,9 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
 TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2 := true
+
+PRODUCT_PACKAGES += \
+    androidx.window.extensions
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_evo/tiramisu/ota.json
