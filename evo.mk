d6dcbd86d20f1c62e82be6bf9a1097e5147cdb49
Alberto Ponces <ponces26@gmail.com>
2023-09-14 12:46:33 +0100

    rework: Nuke fuseblk genfs_context

--- evo.mk
+++ evo.mk
@@ -9,8 +9,5 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_SUPPORTS_QUICK_TAP := true
 
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
-TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
-
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_evo/tiramisu/ota.json
