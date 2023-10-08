7512972bc16759cacb0fd956ea3c2b84ce13b634
Sapan Gajjar <95901240+isg32@users.noreply.github.com>
2023-10-08 15:07:38 +0530

    Update evo.mk

--- evo.mk
+++ evo.mk
@@ -10,4 +10,4 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_evo/tiramisu/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/isg32/treble_build_evo/udc/ota.json
